71be3aaa5ecdd60cfd5d02758cf55df7d8d81b2f
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-11-24 00:37:04 +0900

    Introduce aws-amplify

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import configureStore from './app/store/configureStore';
 import './index.css';
 import App from './app/components/App';
 import registerServiceWorker from './registerServiceWorker';
+import configureAuth from './configureAuth';
+import { withAuthenticator } from 'aws-amplify-react';
 
 const store = configureStore();
 
+configureAuth();
+const AppWithAuthenticator = withAuthenticator(App);
+
 render(
   <Provider store={store}>
-    <App />
+    <AppWithAuthenticator />
   </Provider>,
   document.getElementById('root'),
 );
