b7f543d532419c4877d6a82308c26496cdcae069
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-11-24 07:41:03 +0900

    Persist redux store

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,12 +1,20 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistCombineReducers } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
+import localForage from 'localforage';
 import reducers from '../reducers';
 import rootSaga from '../sagas/rootSaga';
 
+const persistConfig = {
+  key: 'root',
+  storage: localForage,
+};
+
 export default function configureStore() {
   const sagaMiddleware = createSagaMiddleware();
-  const rootReducer = combineReducers(reducers);
+  const rootReducer = persistCombineReducers(persistConfig, reducers);
   const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+  const persistor = persistStore(store);
   sagaMiddleware.run(rootSaga);
-  return store;
+  return { store, persistor };
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/es/integration/react';
 import configureStore from './app/store/configureStore';
 import './index.css';
 import App from './app/components/App';
@@ -8,14 +9,20 @@ import registerServiceWorker from './registerServiceWorker';
 import configureAuth from './configureAuth';
 import { withAuthenticator } from 'aws-amplify-react';
 
-const store = configureStore();
+const { store, persistor } = configureStore();
 
 configureAuth();
 const AppWithAuthenticator = withAuthenticator(App);
 
+function Loading() {
+  return <div>Loading...</div>;
+}
+
 render(
   <Provider store={store}>
-    <AppWithAuthenticator />
+    <PersistGate loading={<Loading />} persistor={persistor}>
+      <AppWithAuthenticator />
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
